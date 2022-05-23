7d1cd7a1a6c13015f4dd87306cb201ec61fe2288
lucasvidela94 <69171039+lucasvidela94@users.noreply.github.com>
2022-05-23 19:26:37 -0300

    refactor(gilded rose class): Refactor update quality method and added new methods
    Co-author by : @EdwardMD <Edwardmi444@gmail.com>
    Co-author by : @V-Juarez <mytacam78@gmail.com>
    Co-author by : @matias.devjam <matias.devjam@gmail.com>

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,11 +2,50 @@
 
 #Class GildedRose
 class GildedRose
+  
   def initialize(items)
+    @new_item = Item.new
     @items = items
   end
 
-  def update_quality; end
+  
+  def update_quality
+    @new_item do |item|
+      case item.name
+      when 'ejemplo uno' then aged_item(item) 
+      when 'ejemplo dos' then legendary(item)
+      when 'ejemplo tres' then backstage(item)
+      end
+    end
+  end
+
+  def aged_item(item)
+    item.quality += item.sell_in <= 0 ? 2 : 1
+    item.quality = 50 if item.quality > 50
+  end 
+
+  def legendary(item)
+    item.quality = 80
+  end
+
+  def conjured
+    item.quality -= 2
+  end
+
+  def backstage(item)
+    case item.sell_in do |item|
+    when item.sell_in <= 10 then item.quality += 2
+    when item.sell_in <= 5  then item.quality += 3
+    end
+    item.quality = 0  if item.sell_in <= 0 
+  end
+
+end
+
+
+
+
+
 
 # Class Item
 class Item
@@ -21,6 +60,7 @@ class Item
   def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
+
 end
 
 puts item1 = Item.new('Japon', 5, 50)
