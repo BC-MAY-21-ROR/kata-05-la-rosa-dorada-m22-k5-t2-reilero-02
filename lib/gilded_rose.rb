e3af6a136787ad6a6e1a45523bb11b777e9e06fa
EdwardMD <Edwardmi444@gmail.com>
2022-05-23 18:14:59 -0500

    refactored update_quality
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @lucasvidela94 <lucas.videla994@gmail.com>
    Co-authored-by: @matias.devjam <matias.devjam@gmail.com>

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,24 +4,24 @@
 class GildedRose
   
   def initialize(items)
-    @new_item = Item.new
     @items = items
   end
 
   
   def update_quality
-    @new_item do |item|
+    @items.each do |item|
       case item.name
       when 'ejemplo uno' then aged_item(item) 
       when 'ejemplo dos' then legendary(item)
       when 'ejemplo tres' then backstage(item)
+      else print(item)
       end
     end
   end
 
   def aged_item(item)
     item.quality += item.sell_in <= 0 ? 2 : 1
-    item.quality = 50 if item.quality > 50
+    (item.quality > 50)? item.quality = 50: puts(item)
   end 
 
   def legendary(item)
@@ -33,11 +33,12 @@ class GildedRose
   end
 
   def backstage(item)
-    case item.sell_in do |item|
-    when item.sell_in <= 10 then item.quality += 2
-    when item.sell_in <= 5  then item.quality += 3
+    @items.each do |item|
+      case item.sell_in
+      when item.sell_in <= 10 then item.quality += 2
+      when item.sell_in <= 5  then item.quality += 3 
+      end 
     end
-    item.quality = 0  if item.sell_in <= 0 
   end
 
 end
@@ -63,14 +64,12 @@ class Item
 
 end
 
-puts item1 = Item.new('Japon', 5, 50)
-puts item2 = Item.new('Nicaragua', 2, 25)
-puts item3 = Item.new('Pan', 15, 22)
-puts item4 = Item.new('Aged Brie', 10, 10)
-puts item5 = Item.new('Sulfuras, Hand of Ragnaros', 20, 20)
-puts item6 = Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 30)
+puts item1 = Item.new('ejemplo uno', 5, 50)
+puts item2 = Item.new('ejemplo dos', 2, 25)
+puts item3 = Item.new('ejemplo tres', 15, 22)
 
 
-items = [item1, item2, item3, item4, item5, item6]
+
+items = [item1, item2, item3]
 rose = GildedRose.new(items)
 puts rose.update_quality
