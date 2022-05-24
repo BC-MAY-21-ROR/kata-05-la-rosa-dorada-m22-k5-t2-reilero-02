31f160f77dc290284dbfd0263161d4526d76ea9a
MatiasDevJam <matias.devjam@gmail.com>
2022-05-24 19:09:05 -0300

    Refactor gilded_rose.rb Co-authored-by: @EdwardMD <Edwardmi444@gmail.com> Co-authored-by: @V-Juarez <mytacam78@gmail.com> Co-authored-by: @lucasvidela94 <lucas.videla994@gmail.com>

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,27 +1,7 @@
 # frozen_string_literal: true
-require 'rspec'
-require_relative'../lib/gilded_rose'
 
+require_relative 'gilded_rose'
 
 describe GildedRose do
-  
-  
-  describe GildedRose do
-    
-    before(:each) do
-      @items = Item.new
-      @rose = GildedRose.new(@items)
-    end
-
-    describe '#update_quality' do     
-    
-    list_items = [example_item,example_itemTwo,example_itemThree]
-    
-    Test_rose = GildedRose.new.update_quality
-    Test_rose(list_items)
-
-    it "aged item increases by two if the sell in days havent passed " do
-      expect(list_items[0].quality).to eql(12)
-    end
-  end
+  describe
 end
